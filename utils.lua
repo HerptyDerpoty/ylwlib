a5a28c6ec4e54e6792fd4db782f9190c0bbaee1c
HerptyDerpoty <herptyderpoty@gmail.com>
2025-05-07 18:12:30 -0400

    initial

---
+++ utils.lua
@@ -0,0 +1,24 @@
+require("util")
+-- removes an array of effects from a table, pass table as array and element as to_be_removed
+function remove_table_element(array, to_be_removed)
+    for _, i in pairs(to_be_removed) do
+        for index, value in pairs(array) do
+            if util.table.compare(value, i) then
+                table.remove(array, index)
+                break
+            end
+        end
+    end
+end
+
+--same function but for array of strings
+function Remove_value(array, to_be_removed)
+    for _, i in pairs(to_be_removed) do
+        for index, value in pairs(array) do
+            if value == i then
+                table.remove(array, index)
+                break
+            end
+        end
+    end
+end
