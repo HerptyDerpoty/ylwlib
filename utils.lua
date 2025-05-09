c316e9340addd3ae26fbf3ef5cc75ccbbd89730e
HerptyDerpoty <herptyderpoty@gmail.com>
2025-05-08 21:46:53 -0400

    changes fucntions to be locals that can only be called by requiring ylwlib instead of exposed as globals.

--- utils.lua
+++ utils.lua
@@ -1,24 +1,26 @@
 require("util")
--- removes an array of effects from a table, pass table as array and element as to_be_removed
-function remove_table_element(array, to_be_removed)
-    for _, i in pairs(to_be_removed) do
-        for index, value in pairs(array) do
-            if util.table.compare(value, i) then
-                table.remove(array, index)
-                break
-            end
-        end
-    end
-end
 
---same function but for array of strings
-function remove_table_element_string(array, to_be_removed)
-    for _, i in pairs(to_be_removed) do
-        for index, value in pairs(array) do
-            if value == i then
-                table.remove(array, index)
-                break
+local utils = {}
+        -- removes an array of effects from a table, pass table as array and element as to_be_removed
+    function utils.remove_table_element(array, to_be_removed)
+            for _, i in pairs(to_be_removed) do
+                for index, value in pairs(array) do
+                    if util.table.compare(value, i) then
+                        table.remove(array, index)
+                        break
+                    end
+                end
             end
         end
-    end
-end
+        --same function but for array of strings
+    function utils.remove_table_element_string(array, to_be_removed)
+            for _, i in pairs(to_be_removed) do
+                for index, value in pairs(array) do
+                    if value == i then
+                        table.remove(array, index)
+                        break
+                    end
+                end
+            end
+        end
+return utils
